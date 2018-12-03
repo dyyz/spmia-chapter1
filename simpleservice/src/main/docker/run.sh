8380afc0beb496c8ed7f359463042864ad1b82fc
dy <944801244@qq.com>
2018-12-03 16:21:52 +0800

    Update run.sh

--- simpleservice/src/main/docker/run.sh
+++ simpleservice/src/main/docker/run.sh
@@ -2,4 +2,4 @@
 echo "********************************************************"
 echo "Starting simple-service "
 echo "********************************************************"
-java -jar /usr/local/simple-service/simple-service-0.0.1-SNAPSHOT.jar
+java -jar /usr/local/simple-service/simple-service-0.0.1-SNAPSHOT.jar --server.port=8088
